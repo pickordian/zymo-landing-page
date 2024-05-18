0110123e26fc64bfc15f4b1be6820752be3ff2c1
pickordian <pxtran7@gmail.com>
2024-05-18 11:33:03 +0700

    finish setting up the site

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# zymo-landing-page
+# Zymo Landing Page
+This project is a simple landing page designed as part of a front-end developer assessment for Zymo Research Cooperation. The page is built using HTML, CSS, and JavaScript.
+
+## Prerequisites
+Before you begin, ensure you have the following:
+- A reliable internet connection to load website assets
+
+## Installation
+To set up and run this project locally, follow these steps:
+
+1. **Clone the repository:**
+   ```sh
+   git clone https://github.com/pickordian/zymo-landing-page.git
+   ```
+2. **Navigate to project repository**
+    ```sh
+    cd zymo-landing-page
+    ```
+3. **Open the `index.html` file in your browser**
+    You can double click on the index.html file, or 
+     You can open it using the following command:
+    ```sh
+    open index.html
+    ```
+**Features
+Comming soon / italic
+**Demo 
+ Click [here](pickordian.github.io/zymo-landing-page) to see the website demo
---
+++ cards.js
@@ -0,0 +1,26 @@
+const cardJSON = {            
+  "card_1": {
+    "title": "Microbiomics",
+    "description": "At Zymo Research we strive to empower microbiome researchers worldwide to achieve their research goals.",
+    "image_url": "https://www.zymoresearch.com/cdn/shop/articles/blog-microbiome_standard_banner_mobile_grande.jpg",
+    "url": "https://www.zymoresearch.com/pages/microbiome-learning-center"
+  },
+  "card_2": {
+    "title": "Sample Collection",
+    "description": "Sample quality is critical to the accuracy and reliability of research and diagnostic results.",
+    "image_url": "https://www.zymoresearch.com/cdn/shop/articles/banner_card_grande.png",
+    "url": "https://www.zymoresearch.com/pages/sample-collection-learning-center"
+  },
+  "card_3": {
+    "title": "DNA Purification",
+    "description": "Zymo Research's DNA purification products are specifically designed to meet the complexities of today.",
+    "image_url": "https://www.zymoresearch.com/cdn/shop/articles/blog-ctab_protocol_for_isolating_dna_from_plant_tissues-featured_image_grande.jpg",
+    "url": "https://www.zymoresearch.com/pages/dna-learning-center"
+  },
+  "card_4": {
+    "title": "RNA Purification",
+    "description": "Zymo Research recognizes the pivotal role of RNA as a central biological molecule in regulating biological pathways.",
+    "image_url": "https://www.zymoresearch.com/cdn/shop/articles/mRNA_Featured_Image_grande.png",
+    "url": "https://www.zymoresearch.com/pages/rna-learning-center"
+  }
+}
