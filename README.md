9f78b4e4bf49933ec91b1abde7b04b2b71a6cb9e
pickordian <pxtran7@gmail.com>
2024-05-20 18:33:06 +0700

    some transition effect added, completed README

--- README.md
+++ README.md
@@ -1,29 +1,59 @@
 # Zymo Landing Page
+
 This project is a simple landing page designed as part of a front-end developer assessment for Zymo Research Cooperation. The page is built using HTML, CSS, and JavaScript.
 
 ## Prerequisites
+
 Before you begin, ensure you have the following:
-- A reliable internet connection to load website assets
+
+- A reliable internet connection to load website's assets
+- Visual Studio Code with Live Server extension installed,
+  or Node.js and npm
 
 ## Installation
+
 To set up and run this project locally, follow these steps:
 
-1. **Clone the repository:**
-   ```sh
-   git clone https://github.com/pickordian/zymo-landing-page.git
-   ```
-2. **Navigate to project repository:**
+### Step 1:Clone the repository:
+
+```sh
+git clone https://github.com/pickordian/zymo-landing-page.git
+```
+
+### Step 2: Navigate to project repository:
+
     ```sh
     cd zymo-landing-page
     ```
-3. **Open the `index.html` file in your browser:**
-    You can double click on the index.html file, or  
-    
-    You can open it using the following command:
+
+### Step 3: On the same terminal, run this project on a local server:
+
+    You do the this step by using either:
+
+#### Visual Studio Code Live Server Extension:
+1. Open this project on Visual Studio Code:
     ```sh
-    open index.html
+    code . 
     ```
+2. Right click on the 'index.html' file
+3. Select 'Open with Live Server'
+4. Open a browser and go to 'http://localhost:5500'
+
+#### Node.js http-server package
+1. Install the 'http-server' package:
+    ```sh
+    npm install -g http-server
+    ```
+2. Start the 'http-server'
+    ```sh
+    http-server
+    ```
+3. Open a browser and go to 'http://localhost:8080'
+
 ## Features
-_Comming soon..._ 
-## Demo 
- Click <a href="https://pickordian.github.io/zymo-landing-page" target="_blank">here</a> to see the website demo
+
+_Comming soon..._
+
+## Demo
+
+Click <a href="https://pickordian.github.io/zymo-landing-page" target="_blank">here</a> to see the website demo
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import cardJSON from "./cards.js";
 import CardJSON from "./cards.js";
 
+// event handler function
+
 function handleHeroInqClick() {
   document.getElementById("Contact").scrollIntoView({
     behavior: "smooth",
@@ -9,45 +11,60 @@ function handleHeroInqClick() {
 }
 
 function insertCards() {
-  let newCards ='';
+
   const cardsSection = document.getElementById('Cards');
   // extract object key: eg: 'card_1, card_2' then
   // use them to access CardJSON data 
   Object.keys(CardJSON).forEach(function (key, index) {
-
+    
+    
+  
     const cardContainer = document.createElement("div");
     cardContainer.classList.add('card-container')
-
+    // inside card container
     const cardLink = document.createElement("a");
     cardLink.classList.add('card');
     cardLink.href = CardJSON[key].url;
     cardLink.target = '_blank';
-
-    const cardImg = document.createElement("img")
+    cardLink.title = `Go to ${cardJSON[key].title} page`;
+      // inside card
+        // inside img-container
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add('img-container');
+    
+    const cardImg = document.createElement("img");
     cardImg.src = CardJSON[key].image_url;
     cardImg.alt = `Zymo's ${CardJSON[key].title} illustration`;
-
-    const cardInfo = document.createElement('div');
-
+    cardImg.loading ='lazy';
+       
+    const card = document.createElement('div');
+        // inside div
     const cardTitle = document.createElement("h3");
     cardTitle.textContent = CardJSON[key].title;
 
     const cardPara = document.createElement("p");
     cardPara.textContent = CardJSON[key].description;
-
+        // end div
+      // end card-link
     const cardCta = document.createElement("a");
     cardCta.classList.add('card-cta');
+    cardCta.classList.add('btn-eff');
     cardCta.href = CardJSON[key].url;
     cardCta.target = '_blank';
     cardCta.textContent = 'Learn more';
+    cardCta.title = `Go to ${cardJSON[key].title} page`;
+    // end card-container
 
-    cardInfo.appendChild(cardTitle);
-    cardInfo.appendChild(cardPara);
-    cardLink.appendChild(cardImg);
-    cardLink.appendChild(cardInfo);
+    // add all tags to create final card
+    card.appendChild(cardTitle);
+    card.appendChild(cardPara);
+    imgContainer.appendChild(cardImg);
+    cardLink.appendChild(imgContainer);
+    cardLink.appendChild(card);
     cardContainer.appendChild(cardLink);
     cardContainer.appendChild(cardCta);
 
+    // append final card to the DOM
     cardsSection.appendChild(cardContainer);
 
   });
@@ -55,7 +72,6 @@ function insertCards() {
 
 function handleContactFormSubmit(event){
   event.preventDefault();
-  
   const formInputs = event.target.querySelectorAll('input');
   formInputs.forEach((input)=>{
     if(input.value === ''){
@@ -68,6 +84,7 @@ function handleContactFormSubmit(event){
   if(OldSucMes){
     OldSucMes.remove();
   }
+  // adding new success message
   const formSubmittedMes = document.createElement('span');
   formSubmittedMes.textContent = 'Your form has been successfully submitted!';
   formSubmittedMes.classList.add('success-message');
@@ -81,6 +98,7 @@ function handleContactFormSubmit(event){
   })
 
 }
+// listeners 
 const heroInqBtn = document.getElementById("Hero-inq-btn");
 heroInqBtn.addEventListener("click", handleHeroInqClick);
 window.onload = insertCards;
